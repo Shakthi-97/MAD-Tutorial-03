823901e5e076e53ebcbd12053ccfbb6655e4391d
Jeyasakthi <it19042848@my.sliit.lk>
2020-08-18 20:58:44 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String Extra_Number1 = "com.IntentsProj.application.IntentsProj.Extra_Number1";
+    public static final String Extra_Number2 = "com.IntentsProj.application.IntentsProj.Extra_Number2";
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnok);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity();
+            }
+
+            public void openSecondActivity() {
+
+                EditText num1 = (EditText)findViewById(R.id.num1) ;
+                int number1 = Integer.parseInt(num1.getText().toString());
+
+                EditText num2 = (EditText)findViewById(R.id.num2) ;
+                int number2 = Integer.parseInt(num2.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this , SecondActivity.class);
+                intent.putExtra(Extra_Number1,number1);
+                intent.putExtra(Extra_Number2,number2);
+                startActivity(intent);
+
+                Toast toast = Toast.makeText(MainActivity.this, "You just clicked the OK button", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.BOTTOM, 0, 0);
+                toast.show();
+
+
+                LayoutInflater li = getLayoutInflater();
+                View layout = li.inflate(R.layout.customtoast, (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+                Toast toast1 = new Toast(getApplicationContext());
+                toast1.setDuration(Toast.LENGTH_SHORT);
+                toast1.setGravity(Gravity.BOTTOM, 0, 0);
+                toast1.setView(layout);
+                toast1.show();
+
+
+            }
+
+        });
+
+    }
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -0,0 +1,119 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    private EditText opr1;
+    private EditText opr2;
+    protected Button btnadd;
+    protected Button btnsub;
+    protected Button btnmul;
+    protected Button btndiv;
+    private TextView txtresult;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int number1 = intent.getIntExtra(MainActivity.Extra_Number1,0);
+        int number2 = intent.getIntExtra(MainActivity.Extra_Number2,0);
+
+        EditText num1 = (EditText)findViewById(R.id.num1) ;
+        EditText num2 = (EditText)findViewById(R.id.num2) ;
+
+        num1.setText("" +number1);
+        num2.setText("" +number2);
+
+
+
+        opr1   = (EditText) findViewById(R.id.num1);
+        opr2   = (EditText) findViewById(R.id.num2);
+        btnadd = (Button) findViewById(R.id.btnadd);
+        btnsub = (Button) findViewById(R.id.btnsub);
+        btnmul = (Button) findViewById(R.id.btnmul);
+        btndiv = (Button) findViewById(R.id.btndiv);
+        txtresult= (TextView) findViewById(R.id.txtresult);
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double no1 = Double.parseDouble(opr1.getText().toString());
+                    double no2 = Double.parseDouble(opr2.getText().toString());
+                    double results = no1 + no2;
+                    txtresult.setText(Double.toString(results));
+                }
+
+            }
+        });
+
+
+
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double no1 = Double.parseDouble(opr1.getText().toString());
+                    double no2 = Double.parseDouble(opr2.getText().toString());
+                    double results = no1 - no2;
+                    txtresult.setText(Double.toString(results));
+                }
+
+            }
+        });
+
+
+        btnmul.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double no1 = Double.parseDouble(opr1.getText().toString());
+                    double no2 = Double.parseDouble(opr2.getText().toString());
+                    double results = no1 * no2;
+                    txtresult.setText(Double.toString(results));
+                }
+
+            }
+        });
+
+
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double no1 = Double.parseDouble(opr1.getText().toString());
+                    double no2 = Double.parseDouble(opr2.getText().toString());
+                    double results = no1 / no2;
+                    txtresult.setText(Double.toString(results));
+                }
+
+            }
+        });
+
+
+    }
+}
